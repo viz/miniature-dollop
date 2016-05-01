2a4ac1f4a4f6978dc52ac7f522f6db74a3067b4a
Pete Whitfield <peter@lawpath.com.au>
2016-05-01 11:20:10 +0000

    minor fixes

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -6,18 +6,21 @@ const Header = () => <Navbar>
   <Navbar.Header>
     <Navbar.Brand>
       <IndexLinkContainer to={{ pathname: '/' }}>
-        <a href='#'>Setup</a>
+        <a>Setup</a>
       </IndexLinkContainer>
     </Navbar.Brand>
+    <Navbar.Toggle />
   </Navbar.Header>
-  <Nav>
-    <LinkContainer to={{ pathname: '/' }}>
-      <NavItem eventKey={1} href='#'>Home</NavItem>
-    </LinkContainer>
-    <LinkContainer to={{ pathname: '/about' }}>
-      <NavItem eventKey={2} href='#'>About</NavItem>
-    </LinkContainer>
-  </Nav>
+  <Navbar.Collapse>
+    <Nav>
+      <LinkContainer to={{ pathname: '/' }}>
+        <NavItem eventKey={1} href='#'>Home</NavItem>
+      </LinkContainer>
+      <LinkContainer to={{ pathname: '/about' }}>
+        <NavItem eventKey={2} href='#'>About</NavItem>
+      </LinkContainer>
+    </Nav>
+  </Navbar.Collapse>
 </Navbar>
 
 export default Header
--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,41 @@
+import { createDevTools } from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+
 import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory, IndexRoute, Router, Route } from 'react-router'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './main.css'
 
 import App from './components/app'
 import Home from './components/home'
 import About from './components/about'
+import reducer from './reducer.js'
+
+// const DevTools = createDevTools(
+//   <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
+//     <LogMonitor theme='tomorrow' preserveScrollTop={false} />
+//   </DockMonitor>
+// )
+
+const store = createStore(
+  reducer
+  // DevTools.instrument()
+)
 
 const div = document.createElement('div')
-
 document.body.appendChild(div)
 
-render(<Router history={browserHistory}>
-  <Route path='/' component={App}>
-    <IndexRoute component={Home} />
-    <Route path='about' component={About} />
-  </Route>
-</Router>, div)
+render(<Provider store={store}>
+  <div>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='about' component={About} />
+      </Route>
+    </Router>
+  </div>
+</Provider>, div)
